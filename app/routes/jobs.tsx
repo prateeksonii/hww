8581a32f5ad48377a57d832014adb9b096b63589
Prateek <prateek_soni@outlook.com>
2022-04-03 15:24:45 +0530

    reposition search
    Signed-off-by: Prateek <prateek_soni@outlook.com>

--- app/routes/jobs.tsx
+++ app/routes/jobs.tsx
@@ -1,7 +1,6 @@
 import type { FC } from "react";
 import { HiSearch } from "react-icons/hi";
 import { ActionFunction, Form, Outlet, redirect } from "remix";
-import { db } from "~/utils/db.server";
 
 export const action: ActionFunction = async ({ request }) => {
   const data = await request.formData();
@@ -17,22 +16,21 @@ const JobsPage: FC = () => {
     <main className="py-8 px-8 mx-auto">
       <h1 className="font-bold text-4xl">Jobs</h1>
       <div className="py-8 grid grid-cols-[20%_80%] gap-8">
-        <div className="shadow-2xl h-full p-4 rounded">
+        <div className="shadow-2xl h-full p-4 rounded"></div>
+        <div className="px-4">
           <Form action="." method="post">
             <label htmlFor="search" className="relative">
               <input
                 type="text"
                 name="search"
                 placeholder="Search jobs"
-                className="bg-primary-dark p-2 text-lg w-full rounded"
+                className="bg-primary-dark p-3 text-xl w-full rounded"
               />
               <button type="submit">
                 <HiSearch className="absolute right-5 top-0 text-lg" />
               </button>
             </label>
           </Form>
-        </div>
-        <div>
           <Outlet />
         </div>
       </div>
