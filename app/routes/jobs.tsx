e935dae495406ee84bca14b6884902aa689fbe83
Prateek <prateek_soni@outlook.com>
2022-04-03 18:38:20 +0530

    update seed script
    Signed-off-by: Prateek <prateek_soni@outlook.com>

--- app/routes/jobs.tsx
+++ app/routes/jobs.tsx
@@ -1,6 +1,23 @@
+import { Country } from "@prisma/client";
 import type { FC } from "react";
 import { HiSearch } from "react-icons/hi";
-import { ActionFunction, Form, Outlet, redirect } from "remix";
+import {
+  ActionFunction,
+  Form,
+  json,
+  LoaderFunction,
+  Outlet,
+  redirect,
+  useLoaderData,
+} from "remix";
+import { db } from "~/utils/db.server";
+
+export const loader: LoaderFunction = async () => {
+  const countries = await db.country.findMany();
+  return json({
+    countries,
+  });
+};
 
 export const action: ActionFunction = async ({ request }) => {
   const data = await request.formData();
@@ -8,16 +25,41 @@ export const action: ActionFunction = async ({ request }) => {
 
   const searchParams = new URLSearchParams({ q: search });
 
+  if (!search || search === "") {
+    return redirect(`/jobs/1`);
+  }
+
   return redirect(`/jobs/search?${searchParams}`);
 };
 
 const JobsPage: FC = () => {
+  const { countries } = useLoaderData() as { countries: Country[] };
+
   return (
     <main className="py-8 px-8 mx-auto">
       <h1 className="font-bold text-4xl">Jobs</h1>
       <div className="py-8 grid grid-cols-[20%_80%] gap-8">
-        <div className="shadow-2xl h-full p-4 rounded"></div>
-        <div className="px-4">
+        <div className="shadow-2xl h-full p-4 rounded">
+          <menu className="w-full p-2">
+            <h2 className="text-2xl mb-8">Filters</h2>
+            <div className="shadow">
+              <label
+                htmlFor="countries"
+                className="text-lg font-bold block mb-2"
+              >
+                Countries
+              </label>
+              <select multiple className="bg-dark w-full scrollbar">
+                {countries.map((country) => (
+                  <option value={country.id} key={country.id}>
+                    {country.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </menu>
+        </div>
+        <div className="px-8">
           <Form action="." method="post">
             <label htmlFor="search" className="relative">
               <input
@@ -26,6 +68,7 @@ const JobsPage: FC = () => {
                 placeholder="Search jobs"
                 className="bg-primary-dark p-3 text-xl w-full rounded"
               />
+              {/* <input type="hidden" value="" name="searchBtn" /> */}
               <button type="submit">
                 <HiSearch className="absolute right-5 top-0 text-lg" />
               </button>
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,41 +1,87 @@
 import { db } from "~/utils/db.server";
 import jobs from "../services/csvParser/jobs.json";
 
-// const db = new PrismaClient({ log: ["query", "info", "warn", "error"] });
-
 async function seed() {
-  for (let i = 0; i < getJobs().length; i++) {
-    const job = getJobs()[i];
-    await db.job.create({
-      data: {
-        name: job.name,
-        remote: job.remote,
-        url: job.url,
-        process: job.process,
-        cities: {
-          connectOrCreate: job.cities.map((city) => ({
-            create: city,
-            where: { name: city.name },
-          })),
+  await db.job.deleteMany();
+  await db.keyword.deleteMany();
+  await db.city.deleteMany();
+  await db.country.deleteMany();
+
+  await db.country.createMany({ data: getCountries(), skipDuplicates: true });
+  await db.city.createMany({ data: getCities(), skipDuplicates: true });
+  await db.keyword.createMany({ data: getKeywords(), skipDuplicates: true });
+  await Promise.all(
+    getJobs().map((job) =>
+      db.job.create({
+        data: {
+          name: job.name,
+          remote: job.remote,
+          url: job.url,
+          process: job.process,
+          cities: {
+            connectOrCreate: job.cities.map((city) => ({
+              create: city,
+              where: { name: city.name },
+            })),
+          },
+          countries: {
+            connectOrCreate: job.countries.map((country) => ({
+              create: country,
+              where: { name: country.name },
+            })),
+          },
+          keywords: {
+            connectOrCreate: job.keywords.map((keyword) => ({
+              create: keyword,
+              where: { name: keyword.name },
+            })),
+          },
         },
-        countries: {
-          connectOrCreate: job.countries.map((country) => ({
-            create: country,
-            where: { name: country.name },
-          })),
-        },
-        keywords: {
-          connectOrCreate: job.keywords.map((keyword) => ({
-            create: keyword,
-            where: { name: keyword.name },
-          })),
-        },
-      },
-    });
-  }
+      })
+    )
+  );
 }
 
 seed();
+
+function getCountries() {
+  let countries = jobs.reduce((countries: { name: string }[], job) => {
+    return [
+      ...countries,
+      ...(job.Countries?.split(",").map((country) => ({
+        name: country.trim(),
+      })) || []),
+    ];
+  }, []);
+
+  return countries;
+}
+
+function getCities() {
+  let cities = jobs.reduce((cities: { name: string }[], job) => {
+    return [
+      ...cities,
+      ...(job.Cities?.split(",").map((city) => ({
+        name: city.trim(),
+      })) || []),
+    ];
+  }, []);
+
+  return cities;
+}
+
+function getKeywords() {
+  let keywords = jobs.reduce((keywords: { name: string }[], job) => {
+    return [
+      ...keywords,
+      ...(job.Keywords?.split(",").map((keyword) => ({
+        name: keyword.trim(),
+      })) || []),
+    ];
+  }, []);
+
+  return keywords;
+}
 
 function getJobs() {
   return jobs.map((job) => {
